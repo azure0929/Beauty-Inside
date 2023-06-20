549e04db73ed19bb2ba52935dcde23aeaffdd8a5
azure0929 <chary8028@naver.com>
2023-06-20 20:32:37 +0900

    Feat: 메인 페이지 초기설정

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -3,16 +3,12 @@ import styled from 'styled-components'
 
 const Main = () => {
 
-  const Title = styled.h1`
-    font-family: 'Noto Sans KR';
-    text-align: center;
-    margin-top: 100px;
-  `
+  
 
   return (
     <div>
       <GlobalStyle />
-      <Title>메인</Title>
+      
     </div>
   )
 }
