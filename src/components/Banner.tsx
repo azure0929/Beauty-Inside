4d4e4114ccb5ce70e1680786f9b1666bb4eef653
azure0929 <chary8028@naver.com>
2023-06-20 20:58:21 +0900

    Feat: banner 이미지 작업

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,8 @@
 import GlobalStyle from '../styles/GlobalStyles'
 import styled from 'styled-components'
+import banner01 from '../../public/assets/main/banner/banner01.jpg'
+import banner02 from '../../public/assets/main/banner/banner02.jpg'
+import banner03 from '../../public/assets/main/banner/banner03.jpg'
 
 const Banner = () => {
 
@@ -7,14 +10,16 @@ const Banner = () => {
     position: relative;
   `
 
+  
+
   return (
     <div>
       <GlobalStyle />
       <Banner>
         <div className='inner'>
           <ProductLink href='javascript:void(0)'><img src={banner01}/></ProductLink>
-          <ProductLink href='javascript:void(0)'><img src={banner01}/></ProductLink>
-          <ProductLink href='javascript:void(0)'><img src={banner01}/></ProductLink>
+          <ProductLink href='javascript:void(0)'><img src={banner02}/></ProductLink>
+          <ProductLink href='javascript:void(0)'><img src={banner03}/></ProductLink>
         </div>
       </Banner>
     </div>
